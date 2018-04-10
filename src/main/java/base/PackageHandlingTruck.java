823755c58fab4fa8763bba143cc03cd817cc1f80
Ehsan Zabardast <ehsanzabardast@gmail.com>
2018-04-10 10:44:01 +0200

    codes for following the line

--- src/main/java/base/PackageHandlingTruck.java
+++ src/main/java/base/PackageHandlingTruck.java
@@ -6,13 +6,18 @@ import java.net.Socket;
 
 import ev3dev.actuators.lego.motors.EV3LargeRegulatedMotor;
 import ev3dev.actuators.lego.motors.EV3MediumRegulatedMotor;
+import ev3dev.sensors.ev3.EV3ColorSensor;
 import ev3dev.sensors.Battery;
 import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
 import lejos.utility.Delay;
 
-public class PackageHandlingTruck {
+import lejos.robotics.SampleProvider;
+
+public class PackageHandlingTruck{
 
     static boolean isRunning = true;
+
 
     public static void main(final String[] args) throws IOException{
 		
@@ -23,8 +28,196 @@ public class PackageHandlingTruck {
         final EV3LargeRegulatedMotor motorLeft = new EV3LargeRegulatedMotor(MotorPort.B);
         final EV3LargeRegulatedMotor motorRight = new EV3LargeRegulatedMotor(MotorPort.C);
         //final EV3MediumRegulatedMotor liftMotor = new EV3MediumRegulatedMotor(MotorPort.A);
+        //final EV3ColorSensor obstacleDetection = new EV3ColorSensor(SensorPort.S2);
+        final EV3ColorSensor lineReader = new EV3ColorSensor(SensorPort.S1);
 
-        System.out.println("Motors initialized");
+        motorLeft.setSpeed(200);
+        motorRight.setSpeed(200);
+        motorLeft.backward();
+        motorRight.backward();
+
+        SampleProvider lineSample = lineReader.getRedMode();
+        int sampleSize = lineSample.sampleSize();
+        float[] sample = new float[sampleSize];
+        boolean stopFlag = false;
+
+        try
+        {
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+        while (!stopFlag) {
+            lineSample.fetchSample(sample, 0);
+            if ((int)sample[0] <= 18){
+                motorLeft.setSpeed(200);
+                motorRight.setSpeed(10);
+                motorLeft.backward();
+                motorRight.backward();
+            }
+            else if (18 < (int)sample[0] && (int)sample[0] <= 25){
+                motorLeft.setSpeed(200);
+                motorRight.setSpeed(60);
+                motorLeft.backward();
+                motorRight.backward();
+            }
+            else if (25 < (int)sample[0] && (int)sample[0] <= 29){
+                motorLeft.setSpeed(200);
+                motorRight.setSpeed(150);
+                motorLeft.backward();
+                motorRight.backward();
+            }
+            else if (29 < (int)sample[0] && (int)sample[0] < 31){
+                motorLeft.setSpeed(200);
+                motorRight.setSpeed(200);
+                motorLeft.backward();
+                motorRight.backward();
+            }
+            else if (31 <= (int)sample[0] && (int)sample[0] < 35){
+                motorLeft.setSpeed(150);
+                motorRight.setSpeed(200);
+                motorLeft.backward();
+                motorRight.backward();
+            }
+            else if (35 <= (int)sample[0] && (int)sample[0] < 42){
+                motorLeft.setSpeed(60);
+                motorRight.setSpeed(200);
+                motorLeft.backward();
+                motorRight.backward();
+            }
+            else if (42 <= (int)sample[0] && (int)sample[0] < 52){
+                motorLeft.setSpeed(10);
+                motorRight.setSpeed(200);
+                motorLeft.backward();
+                motorRight.backward();
+            }
+            else if (52 <= (int)sample[0]){
+                motorLeft.stop(true);
+                motorRight.stop(true);
+                stopFlag = true;
+            }
+            System.out.println("Sample[0]=" +  (int)sample[0]);
+            Delay.msDelay(10);
+        }
+
+
+//////////////////////////////////////////////////
+
+//        Runnable stopMotor = () -> {
+//            //String threadName = Thread.currentThread().getName();
+//            //System.out.println("Hello " + threadName);
+//
+//            SampleProvider sp = obstacleDetection.getRGBMode();
+//
+//            int sampleSize = sp.sampleSize();
+//            float[] sample = new float[sampleSize];
+//            boolean stopFlag = true;
+//
+//            // Takes some samples and prints them
+//            while (stopFlag) {
+//                sp.fetchSample(sample, 0);
+//                sp.fetchSample(sample, 1);
+//                sp.fetchSample(sample, 2);
+//                System.out.println("Sample[0]=" +  (int)sample[0]);
+//                System.out.println("Sample[1]=" +  (int)sample[1]);
+//                System.out.println("Sample[2]=" +  (int)sample[2]);
+//                if ((int)sample[0] > 75 || (int)sample[1] > 75 || (int)sample[2] > 75){
+//                    motorLeft.stop(true);
+//                    motorRight.stop(true);
+//                    stopFlag = false;
+//                }
+//                Delay.msDelay(250);
+//            }
+//        };
+//
+//        motorRight.setSpeed(400);
+//        motorLeft.setSpeed(400);
+//        motorRight.backward();
+//        motorLeft.backward();
+//
+//        stopMotor.run();
+//        Thread stopMotorThread = new Thread(stopMotor);
+//        stopMotorThread.start();
+//
+//        liftMotor.setSpeed(200);
+//        liftMotor.forward();
+//        try
+//        {
+//            Thread.sleep(2000);
+//        }
+//        catch(InterruptedException ex)
+//        {
+//            Thread.currentThread().interrupt();
+//        }
+//        liftMotor.stop(true);
+//
+//        motorRight.setSpeed(400);
+//        motorLeft.setSpeed(400);
+//        motorRight.forward();
+//        motorLeft.forward();
+//        try
+//        {
+//            Thread.sleep(2000);
+//        }
+//        catch(InterruptedException ex)
+//        {
+//            Thread.currentThread().interrupt();
+//        }
+//        motorRight.stop(true);
+//        motorLeft.stop(true);
+//
+//        motorRight.setSpeed(400);
+//        motorLeft.setSpeed(400);
+//        motorRight.forward();
+//        motorLeft.backward();
+//        try
+//        {
+//            Thread.sleep(2800);
+//        }
+//        catch(InterruptedException ex)
+//        {
+//            Thread.currentThread().interrupt();
+//        }
+//        motorRight.stop(true);
+//        motorLeft.stop(true);
+//
+//        motorRight.setSpeed(400);
+//        motorLeft.setSpeed(400);
+//        motorRight.backward();
+//        motorLeft.backward();
+//        try
+//        {
+//            Thread.sleep(2000);
+//        }
+//        catch(InterruptedException ex)
+//        {
+//            Thread.currentThread().interrupt();
+//        }
+//        motorRight.stop(true);
+//        motorLeft.stop(true);
+
+        //////////////////////////////////////////////////
+
+//        liftMotor.rotate(-40);
+
+//        final EV3ColorSensor lineReader = new EV3ColorSensor(SensorPort.S1);
+//        SampleProvider sp = lineReader.getRGBMode();
+//
+//        int sampleSize = sp.sampleSize();
+//        float[] sample = new float[sampleSize];
+//
+//        // Takes some samples and prints them
+//        for (int i = 0; i < 100; i++) {
+//            sp.fetchSample(sample, 0);
+//            System.out.println("N=" + i + " Sample=" +  (int)sample[0]);
+//
+//            Delay.msDelay(500);
+//        }
+
+
+        //System.out.println("Motors initialized");
 
         //https://docs.oracle.com/javase/7/docs/api/java/net/ServerSocket.html
 //        ServerSocket serv = new ServerSocket(19232);
@@ -43,40 +236,59 @@ public class PackageHandlingTruck {
 //        System.out.println("Checking Battery");
 //        System.out.println("Voltage: " + Battery.getInstance().getVoltage());
 
-        motorLeft.setSpeed(500);
-        motorRight.setSpeed(500);
-        motorLeft.backward();
-        motorRight.backward();
 
-        try
-        {
-            Thread.sleep(3000);
-        }
-        catch(InterruptedException ex)
-        {
-            Thread.currentThread().interrupt();
-        }
+//        motorLeft.setSpeed(500);
+//        motorRight.setSpeed(500);
+//        motorLeft.backward();
+//        motorRight.forward();
+//
+//        try
+//        {
+//            Thread.sleep(2500);
+//        }
+//        catch(InterruptedException ex)
+//        {
+//            Thread.currentThread().interrupt();
+//        }
+//
+//        motorLeft.stop(true);
+//        motorRight.stop(true);
 
-        motorLeft.stop(true);
-        motorRight.stop(true);
 
-
-        motorLeft.setSpeed(500);
-        motorRight.setSpeed(500);
-        motorLeft.forward();
-        motorRight.forward();
-
-        try
-        {
-            Thread.sleep(3000);
-        }
-        catch(InterruptedException ex)
-        {
-            Thread.currentThread().interrupt();
-        }
-
-        motorLeft.stop(true);
-        motorRight.stop(true);
+//        motorLeft.setSpeed(500);
+//        motorRight.setSpeed(500);
+//        motorLeft.backward();
+//        motorRight.backward();
+//
+//        try
+//        {
+//            Thread.sleep(3000);
+//        }
+//        catch(InterruptedException ex)
+//        {
+//            Thread.currentThread().interrupt();
+//        }
+//
+//        motorLeft.stop(true);
+//        motorRight.stop(true);
+//
+//
+//        motorLeft.setSpeed(500);
+//        motorRight.setSpeed(500);
+//        motorLeft.forward();
+//        motorRight.forward();
+//
+//        try
+//        {
+//            Thread.sleep(3000);
+//        }
+//        catch(InterruptedException ex)
+//        {
+//            Thread.currentThread().interrupt();
+//        }
+//
+//        motorLeft.stop(true);
+//        motorRight.stop(true);
 
 
 //        liftMotor.setSpeed(300);
