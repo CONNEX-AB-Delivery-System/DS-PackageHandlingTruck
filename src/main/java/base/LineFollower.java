5c39f809f4ba0a83b21ef3ce74dd1d89413e5ea4
Ehsan Zabardast <ehsanzabardast@gmail.com>
2018-04-12 18:00:25 +0200

    MVP

---
+++ src/main/java/base/LineFollower.java
@@ -0,0 +1,52 @@
+package base;
+
+public class LineFollower {
+
+    public static int[] motorsSpeed(int R, int G, int B){
+
+        float average = (R + G + B) / 3;
+        float sensorValue = average * 100 / 255;
+
+        int[] speeds;
+        speeds = new int[2];
+
+        if (sensorValue <= 18){
+//            System.out.println("WARNING 1: Sample[0]=" +  sensorValue);
+            speeds[0] = 200;
+            speeds[1] = 0;
+        }
+        else if (sensorValue > 18 && sensorValue <= 25){
+            speeds[0] = 200;
+            speeds[1] = 60;
+        }
+        else if (sensorValue > 25 && sensorValue <= 29){
+            speeds[0] = 200;
+            speeds[1] = 150;
+        }
+        else if (sensorValue > 29 && sensorValue < 31){
+//            System.out.println("WARNING 3: To the infinity and beyond   =" +  sensorValue);
+            speeds[0] = 200;
+            speeds[1] = 200;
+        }
+        else if (sensorValue >= 31 && sensorValue < 35){
+            speeds[0] = 150;
+            speeds[1] = 200;
+        }
+        else if (sensorValue >= 35 && sensorValue < 42){
+            speeds[0] = 60;
+            speeds[1] = 200;
+        }
+        else if (sensorValue >= 42 && sensorValue < 52){
+            speeds[0] = 0;
+            speeds[1] = 200;
+        }
+//        else if (sensorValue >= 73){
+//            System.out.println("WARNING 2: Sample[0]=" +  sensorValue);
+//            speeds[0] = 0;
+//            speeds[1] = 0;
+////        }
+
+        return speeds;
+    }
+
+}
--- src/main/java/base/PackageHandlingTruck.java
+++ src/main/java/base/PackageHandlingTruck.java
@@ -11,15 +11,14 @@ import ev3dev.sensors.Battery;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
 import lejos.utility.Delay;
-
 import lejos.robotics.SampleProvider;
+import javax.sound.sampled.Line;
 
 public class PackageHandlingTruck{
 
     static boolean isRunning = true;
 
-
-    public static void main(final String[] args) throws IOException{
+    public static void main(final String[] args){
 		
 		//implement this:
 		//https://github.com/ev3dev-lang-java/examples/blob/master/ev3dev-lang-java/src/main/java/lejos/navigation/pathfinding/pathfind.java
@@ -27,91 +26,218 @@ public class PackageHandlingTruck{
         //System.out.println("Creating Motor A & B & C");
         final EV3LargeRegulatedMotor motorLeft = new EV3LargeRegulatedMotor(MotorPort.B);
         final EV3LargeRegulatedMotor motorRight = new EV3LargeRegulatedMotor(MotorPort.C);
-        //final EV3MediumRegulatedMotor liftMotor = new EV3MediumRegulatedMotor(MotorPort.A);
-        //final EV3ColorSensor obstacleDetection = new EV3ColorSensor(SensorPort.S2);
+        final EV3MediumRegulatedMotor liftMotor = new EV3MediumRegulatedMotor(MotorPort.A);
+        final EV3ColorSensor obstacleDetection = new EV3ColorSensor(SensorPort.S2);
         final EV3ColorSensor lineReader = new EV3ColorSensor(SensorPort.S1);
 
-        motorLeft.setSpeed(200);
-        motorRight.setSpeed(200);
-        motorLeft.backward();
-        motorRight.backward();
+//        boolean flag = true;
+//        int counter = 0;
+//        while (flag) {
+//
+//            counter++;
+//            Delay.msDelay(1000);
+//
+//            SampleProvider colorSample = lineReader.getRGBMode();
+//            int sampleSizeColor = colorSample.sampleSize();
+//            float[] sampleColor = new float[sampleSizeColor];
+//            Delay.msDelay(500);
+//            colorSample.fetchSample(sampleColor, 0);
+//            colorSample.fetchSample(sampleColor, 1);
+//            colorSample.fetchSample(sampleColor, 2);
+//
+//            int colorSampleRed = (int) sampleColor[0];
+//            int colorSampleGreen = (int) sampleColor[1];
+//            int colorSampleBlue = (int) sampleColor[2];
+//
+//            float average = (colorSampleRed + colorSampleGreen + colorSampleBlue) / 3;
+//            float normalAverage = average * 100 / 255;
+//
+//            System.out.println("=================================");
+//            System.out.println("Red = " + colorSampleRed);
+//            System.out.println("Green = " + colorSampleGreen);
+//            System.out.println("Blue = " + colorSampleBlue);
+//            System.out.println("---------------------------------");
+//            System.out.println("Average = " + average);
+//            System.out.println("Normalized Average = " + normalAverage);
+//
+//            if (counter > 100){
+//                flag = false;
+//            }
+//        }
 
-        SampleProvider lineSample = lineReader.getRedMode();
-        int sampleSize = lineSample.sampleSize();
-        float[] sample = new float[sampleSize];
+        lineReader.setCurrentMode(2);
         boolean stopFlag = false;
-
-        try
-        {
-            Thread.sleep(3000);
-        }
-        catch(InterruptedException ex) {
-            Thread.currentThread().interrupt();
-        }
+        Delay.msDelay(3000);
 
         while (!stopFlag) {
-            lineSample.fetchSample(sample, 0);
-            if ((int)sample[0] <= 18){
-                System.out.println("WARNING 1: Sample[0]=" +  (int)sample[0]);
-                motorLeft.setSpeed(200);
-                motorRight.setSpeed(0);
-                motorLeft.backward();
-                motorRight.backward();
-            }
-            else if ((18 < (int)sample[0]) && ((int)sample[0] <= 25)){
-                System.out.println("Sample[0]=" +  (int)sample[0]);
-                motorLeft.setSpeed(200);
-                motorRight.setSpeed(60);
-                motorLeft.backward();
-                motorRight.backward();
-            }
-            else if (25 < (int)sample[0] && (int)sample[0] <= 29){
-                motorLeft.setSpeed(200);
-                motorRight.setSpeed(150);
-                motorLeft.backward();
-                motorRight.backward();
-            }
-            else if (29 < (int)sample[0] && (int)sample[0] < 31){
-                System.out.println("WARNING 3: To the infinity and beyond   =" +  (int)sample[0]);
-                motorLeft.setSpeed(300);
-                motorRight.setSpeed(300);
-                motorLeft.backward();
-                motorRight.backward();
-            }
-            else if (31 <= (int)sample[0] && (int)sample[0] < 35){
-                motorLeft.setSpeed(150);
-                motorRight.setSpeed(200);
-                motorLeft.backward();
-                motorRight.backward();
-            }
-            else if (35 <= (int)sample[0] && (int)sample[0] < 42){
-                motorLeft.setSpeed(60);
-                motorRight.setSpeed(200);
-                motorLeft.backward();
-                motorRight.backward();
-            }
-            else if (42 <= (int)sample[0] && (int)sample[0] < 52){
-                motorLeft.setSpeed(0);
-                motorRight.setSpeed(200);
-                motorLeft.backward();
-                motorRight.backward();
-            }
-            else if (42 <= (int)sample[0] && (int)sample[0] < 73){
-                System.out.println("Sample[0]=" +  (int)sample[0]);
-                motorLeft.setSpeed(0);
-                motorRight.setSpeed(150);
-                motorLeft.backward();
-                motorRight.backward();
-            }
-            else if (73 <= (int)sample[0]){
-                System.out.println("WARNING 2: Sample[0]=" +  (int)sample[0]);
+
+            SampleProvider colorSample = lineReader.getRGBMode();
+            int sampleSizeColor = colorSample.sampleSize();
+            float[] sampleColor = new float[sampleSizeColor];
+
+            colorSample.fetchSample(sampleColor, 0);
+            colorSample.fetchSample(sampleColor, 1);
+            colorSample.fetchSample(sampleColor, 2);
+
+            int colorSampleRed = (int)sampleColor[0];
+            int colorSampleGreen = (int)sampleColor[1];
+            int colorSampleBlue = (int)sampleColor[2];
+
+            int[] speed = LineFollower.motorsSpeed(colorSampleRed, colorSampleGreen, colorSampleBlue);
+            motorLeft.setSpeed(speed[0]);
+            motorRight.setSpeed(speed[1]);
+            motorLeft.backward();
+            motorRight.backward();
+
+//            System.out.println("Red = " + colorSampleRed);
+//            System.out.println("Green = " + colorSampleGreen);
+//            System.out.println("Blue = " + colorSampleBlue);
+
+            if (colorSampleRed > 125 && colorSampleBlue < 50 && colorSampleGreen < 50){
                 motorLeft.stop(true);
                 motorRight.stop(true);
                 stopFlag = true;
             }
 
             Delay.msDelay(5);
+
         }
+
+        liftMotor.setSpeed(50);
+        liftMotor.rotateTo(-50, true);
+
+        obstacleDetection.setCurrentMode(2);
+        boolean obstacleFlag = false;
+        Delay.msDelay(3000);
+
+        while (!obstacleFlag) {
+
+            SampleProvider obstacleSample = obstacleDetection.getRGBMode();
+            int obstacleSampleSize = obstacleSample.sampleSize();
+            float[] obstacleSampleColor = new float[obstacleSampleSize];
+            Delay.msDelay(500);
+            obstacleSample.fetchSample(obstacleSampleColor, 0);
+            obstacleSample.fetchSample(obstacleSampleColor, 1);
+            obstacleSample.fetchSample(obstacleSampleColor, 2);
+
+            int obstacleColorSampleRed = (int) obstacleSampleColor[0];
+            int obstacleColorSampleGreen = (int) obstacleSampleColor[1];
+            int obstacleColorSampleBlue = (int) obstacleSampleColor[2];
+
+            float average = (obstacleColorSampleRed + obstacleColorSampleGreen + obstacleColorSampleBlue) / 3;
+
+            if (average < 25) {
+                motorLeft.setSpeed(150);
+                motorRight.setSpeed(150);
+                motorLeft.backward();
+                motorRight.backward();
+            }
+            else{
+                motorLeft.stop(true);
+                motorRight.stop(true);
+                obstacleFlag = true;
+            }
+
+            Delay.msDelay(5);
+        }
+
+        liftMotor.setSpeed(50);
+        liftMotor.rotateTo(70, true);
+
+        Delay.msDelay(3000);
+
+        motorLeft.setSpeed(200);
+        motorRight.setSpeed(200);
+        motorLeft.forward();
+        motorRight.forward();
+        Delay.msDelay(2000);
+        motorLeft.stop(true);
+        motorRight.stop(true);
+
+        lineReader.setCurrentMode(2);
+        boolean turnFlag = false;
+        Delay.msDelay(3000);
+
+        while (!turnFlag){
+
+            SampleProvider turnSample = lineReader.getRGBMode();
+            int turnSampleSizeColor = turnSample.sampleSize();
+            float[] turnSampleColor = new float[turnSampleSizeColor];
+
+            turnSample.fetchSample(turnSampleColor, 0);
+            turnSample.fetchSample(turnSampleColor, 1);
+            turnSample.fetchSample(turnSampleColor, 2);
+
+            int turnColorSampleRed = (int)turnSampleColor[0];
+            int turnColorSampleGreen = (int)turnSampleColor[1];
+            int turnColorSampleBlue = (int)turnSampleColor[2];
+
+            float turnAverage = (turnColorSampleRed + turnColorSampleGreen + turnColorSampleBlue) / 3;
+
+            if ((int)turnAverage < 80){
+                motorLeft.stop(true);
+                motorRight.stop(true);
+                turnFlag = true;
+            }
+            else {
+                motorLeft.setSpeed(150);
+                motorRight.setSpeed(150);
+                motorLeft.forward();
+                motorRight.backward();
+            }
+
+            Delay.msDelay(5);
+        }
+
+        lineReader.setCurrentMode(2);
+        boolean deliveryStopFlag = false;
+        Delay.msDelay(3000);
+
+        while (!deliveryStopFlag) {
+
+            SampleProvider deliveryColorSample = lineReader.getRGBMode();
+            int deliverySampleSizeColor = deliveryColorSample.sampleSize();
+            float[] deliverySampleColor = new float[deliverySampleSizeColor];
+
+            deliveryColorSample.fetchSample(deliverySampleColor, 0);
+            deliveryColorSample.fetchSample(deliverySampleColor, 1);
+            deliveryColorSample.fetchSample(deliverySampleColor, 2);
+
+            int deliveryColorSampleRed = (int)deliverySampleColor[0];
+            int deliveryColorSampleGreen = (int)deliverySampleColor[1];
+            int deliveryColorSampleBlue = (int)deliverySampleColor[2];
+
+            int[] speed = LineFollower.motorsSpeed(deliveryColorSampleRed, deliveryColorSampleGreen, deliveryColorSampleBlue);
+            motorLeft.setSpeed(speed[0]);
+            motorRight.setSpeed(speed[1]);
+            motorLeft.backward();
+            motorRight.backward();
+
+            if (deliveryColorSampleRed < 30 && deliveryColorSampleBlue < 50 && deliveryColorSampleGreen > 70){
+                motorLeft.stop(true);
+                motorRight.stop(true);
+                deliveryStopFlag = true;
+            }
+
+            Delay.msDelay(5);
+
+        }
+
+        liftMotor.setSpeed(50);
+        liftMotor.rotateTo(-90, true);
+
+        Delay.msDelay(1000);
+
+        motorLeft.setSpeed(200);
+        motorRight.setSpeed(200);
+        motorLeft.forward();
+        motorRight.forward();
+        Delay.msDelay(3000);
+        motorLeft.stop(true);
+        motorRight.stop(true);
+
+
+
 
 
 //////////////////////////////////////////////////
