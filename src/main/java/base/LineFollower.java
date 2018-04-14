a08e47e2ab9ed673ab86f622d6e11a9ebbc23a4b
Ehsan Zabardast <ehsanzabardast@gmail.com>
2018-04-14 16:57:41 +0200

    cleaning the code for LineFollower class

--- src/main/java/base/LineFollower.java
+++ src/main/java/base/LineFollower.java
@@ -2,7 +2,7 @@ package base;
 
 public class LineFollower {
 
-    public static int[] motorsSpeed(int R, int G, int B){
+    public static int[] motorsSpeed(int R, int G, int B) {
 
         float average = (R + G + B) / 3;
         float sensorValue = average * 100 / 255;
@@ -10,43 +10,29 @@ public class LineFollower {
         int[] speeds;
         speeds = new int[2];
 
-        if (sensorValue <= 18){
-//            System.out.println("WARNING 1: Sample[0]=" +  sensorValue);
+        if (sensorValue <= 18) {
             speeds[0] = 200;
             speeds[1] = 0;
-        }
-        else if (sensorValue > 18 && sensorValue <= 25){
+        } else if (sensorValue > 18 && sensorValue <= 25) {
             speeds[0] = 200;
             speeds[1] = 60;
-        }
-        else if (sensorValue > 25 && sensorValue <= 29){
+        } else if (sensorValue > 25 && sensorValue <= 29) {
             speeds[0] = 200;
             speeds[1] = 150;
-        }
-        else if (sensorValue > 29 && sensorValue < 31){
-//            System.out.println("WARNING 3: To the infinity and beyond   =" +  sensorValue);
+        } else if (sensorValue > 29 && sensorValue < 31) {
             speeds[0] = 200;
             speeds[1] = 200;
-        }
-        else if (sensorValue >= 31 && sensorValue < 35){
+        } else if (sensorValue >= 31 && sensorValue < 35) {
             speeds[0] = 150;
             speeds[1] = 200;
-        }
-        else if (sensorValue >= 35 && sensorValue < 42){
+        } else if (sensorValue >= 35 && sensorValue < 42) {
             speeds[0] = 60;
             speeds[1] = 200;
-        }
-        else if (sensorValue >= 42 && sensorValue < 52){
+        } else if (sensorValue >= 42 && sensorValue < 52) {
             speeds[0] = 0;
             speeds[1] = 200;
         }
-//        else if (sensorValue >= 73){
-//            System.out.println("WARNING 2: Sample[0]=" +  sensorValue);
-//            speeds[0] = 0;
-//            speeds[1] = 0;
-////        }
 
         return speeds;
     }
-
 }
